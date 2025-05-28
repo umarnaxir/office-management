94562823c13e12ff5b620aae2e9d3d8085f3bf37
umarnaxir <erumarnazir@gmail.com>
2025-05-28 13:01:58 +0530

    Updated

--- src/components/Basics/Sum.js
+++
@@ -1,9 +0,0 @@
-export default function Sum(props) {
-    const addition = props.a + props.b;
-    return (
-        <div style={{color: "black"}}>
-            <p>Adding numbers {props.a} and {props.b} </p>
-            <h1>The result is {addition} </h1>
-        </div>
-    )
-}
